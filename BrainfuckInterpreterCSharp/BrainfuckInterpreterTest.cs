847c11b18a43e2c38d8854ccdaa38b970b3e8bf4
Gleb Kruglov <kruglovgleb@gmail.com>
2018-02-14 23:34:16 +0200

    Tests

---
+++ BrainfuckInterpreterCSharp/BrainfuckInterpreterTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace BrainfuckInterpreterCSharp {
+    public class BrainfuckInterpreterTest : IDisposable {
+        private StringWriter _stringWriter;
+
+        public BrainfuckInterpreterTest() {
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        [Fact]
+        public void BrainfuckInterpreter_HelloWorld() {
+            var interpreter = new BrainfuckInterpreter(BrainfuckExamples.BrainfuckExamples.HelloWorld);
+            interpreter.Parse();
+            interpreter.Run();
+
+            Assert.Equal("Hello World!\n", _stringWriter.ToString());
+        }
+
+        [Fact]
+        public void BrainfuckInterpreter_SimpleAddNumbersOutput() {
+            var interpreter = new BrainfuckInterpreter(BrainfuckExamples.BrainfuckExamples.SimpleAddNumbersOutput);
+            interpreter.Parse();
+            interpreter.Run();
+
+            Assert.Equal("7", _stringWriter.ToString());
+        }
+
+        public void Dispose() {
+            _stringWriter?.Dispose();
+        }
+    }
+}
---
+++ BrainfuckInterpreterCSharpStatic/BrainfuckInterpreterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace BrainfuckInterpreterCSharpStatic {
+    public class BrainfuckInterpreterTest : IDisposable {
+        private StringWriter _stringWriter;
+
+        public BrainfuckInterpreterTest() {
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        [Fact]
+        public void BrainfuckInterpreter_HelloWorld() {
+            BrainfuckInterpreter.Run(BrainfuckExamples.BrainfuckExamples.HelloWorld);
+
+            Assert.Equal("Hello World!\n", _stringWriter.ToString());
+        }
+
+        [Fact]
+        public void BrainfuckInterpreter_SimpleAddNumbersOutput() {
+            BrainfuckInterpreter.Run(BrainfuckExamples.BrainfuckExamples.SimpleAddNumbersOutput);
+
+            Assert.Equal("7", _stringWriter.ToString());
+        }
+
+        public void Dispose() {
+            _stringWriter?.Dispose();
+        }
+    }
+}
